097bcc0b20e8d63dd0421719281749de7e390009
S-create1900 <saphron_m@yahoo.com>
2024-12-28 19:27:06 -0700

    Completed

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -9,24 +9,34 @@ public class FarmerAlBaryardInventoryApplicationTwo {
 	public static void main(String[] args) {
 		FileService fileService = new FileService();
 		Pig[] pigs = fileService.getPigsFromFile(); // Get the array of Pig objects
+		String [] pigNames = new String[4];
 
 		// Check if pigs is null (in case of an error reading the file)
 		if (pigs == null) {
 			System.out.println("No pigs found or there was an error reading the file.");
 			return;
 		}
-
-		// 1. Sort the pigs by name
-		Arrays.sort(pigs, (p1, p2) -> p1.getName().compareTo(p2.getName()));
-
-		// 2. Print sorted pigs
-		for (Pig pig : pigs) {
-			System.out.println(pig.getName()); // Print only each pig's name
+		
+		
+		// 1. run for loop on the pig array
+		for (int i = 0; i < 4; i++) {
+		// 2. in the for loop create an array called pig names, get the names of the pigs using the 
+		//get names getter from the Pig class, 
+			String pigName = pigs[i].getName();
+			//save it into the pigNames string array
+			//System.out.println("Current pig: " + pigName);
+			pigNames[i] = pigName;
 		}
-
+		
+		//3. Sort the pig names: 
+		Arrays.sort(pigNames);
+		for (String pigName: pigNames)
+			System.out.println(pigName);
+		{		
+		
 		// 3. If you want to find the loudest animal based on some other criteria, you
 		// can implement that logic here.
 		// For now, we will just print a message.
 //        System.out.println("\nNote: Loudness attribute is not included in this implementation.");
-	}
+	}}
 }
